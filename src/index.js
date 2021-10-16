9b5d0d9b9cacf0c1e8b659c57fa78fd7e35be85f
hatemhos11 <hatemhosni00@gmail.com>
2021-10-17 00:03:25 +0200

    first

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ const store = createStore(AllReducers , applyMiddleware(thunk))
 
 ReactDOM.render(
 <React.StrictMode>
-  <BrowserRouter basename='/react'>
+  <BrowserRouter>
     <Provider store={store}> 
       <App />
     </Provider>
